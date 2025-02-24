832c6c92727f5a2ece6b12475f99d619fe83f131
AssassinTurkey <toothlessdragon100@gmail.com>
2025-02-24 16:24:02 +1100

    Update html files and added basic signup functionality
    Need to check database if the user already exists.

--- app.py
+++ app.py
@@ -1,28 +1,55 @@
 import sqlite3
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
-
+#Setting up the model, tokeniser, global variables and Flask app
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
 model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
 
+username = ''
+password = ''
+permLevel = 0
+
+
 app = Flask(__name__)
 
-def init_db():
-    conn = sqlite3.connect('.\databases\chat_history.db')
+
+def init_user_db():
+    conn = sqlite3.connect('users.db')
     c = conn.cursor()
     c.execute('''
-              CREATE TABLE IF NOT EXISTS .\databases\chat_history (
-              id INTEGER PRIMARY KEY,
-              user_id INTEGER, 
-              message TEXT,
-              chatNumber INTEGER,
-              chatTitle TEXT,
+                CREATE TABLE IF NOT EXISTS users (
+                user_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                permission_level INTEGER DEFAULT 0 NOT NULL
+               )
+                ''')
+    conn.commit()
+    conn.close()
+
+
+def init_chathistory_db():
+    conn = sqlite3.connect('chat_history.db')
+    c = conn.cursor()
+    c.execute('''
+              CREATE TABLE IF NOT EXISTS chat_history (
+              chat_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+              user_id INTEGER NOT NULL,
+              message TEXT NOT NULL,
+              chatNumber INTEGER NOT NULL,
+              chatTitle TEXT NOT NULL,
+              messageTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+              FOREIGN KEY (user_id) REFERENCES users(user_id)
               )
               ''')
     conn.commit()
     conn.close()
+
+
+init_user_db()
+init_chathistory_db()
 
 
 @app.route('/')
@@ -38,29 +65,53 @@ def chat():
 
 
 
+@app.route('/login', methods=['GET'])
+def login():
+     return render_template('login.html')
+
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+     if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        conn = sqlite3.connect('users.db')
+        c = conn.cursor()
+        c.execute('''
+                    INSERT INTO users (username, password)
+                    VALUES (?, ?)
+                    ''', (username, password))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index')), username, password
+     else:
+        return render_template('signup.html')
+
+
+
+
 
 
 def get_Chat_respone(text):
-    # Let's chat for 5 lines
-    #for step in range(5):
-        # encode the new user input, add the eos_token and return a tensor in Pytorch
-        noOfMessages = 0
-        new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
+    # encode the new user input, add the eos_token and return a tensor in Pytorch
+    noOfMessages = 0
+    new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
 
 
         # append the new user input tokens to the chat history
-        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if noOfMessages > 0 else new_user_input_ids
+    bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if noOfMessages > 0 else new_user_input_ids
 
 
         # generated a response while limiting the total chat history to 1000 tokens, 
-        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+    chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
 
-        noOfMessages += 1
+    noOfMessages += 1
 
-        # pretty print last ouput tokens from bot
-        return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+    # pretty print last ouput tokens from bot
+    return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
     
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
     #get_Chat_respone()
